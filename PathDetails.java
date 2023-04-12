f08e413ae8f88f04846fffa7b638e14582bd86db
LikithMahindra <130539167+LikithMahindra@users.noreply.github.com>
2023-04-12 12:21:12 +0530

    Add files via upload
    BackEnd Java Program Crud Operations

---
+++ PathDetails.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.deepak.backend;
+
+/**
+ *
+ * @author Deepak
+ */
+public class PathDetails 
+{
+    static String PROFILE_PIC_PATH="E:\\Projects\\NetbeansProject\\JobPortal\\build\\web\\profilepics\\";
+    static String RESUME_PATH="E:\\Projects\\NetbeansProject\\JobPortal\\build\\web\\resumes\\";
+}
